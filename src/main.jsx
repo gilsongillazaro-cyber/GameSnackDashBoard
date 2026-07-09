0b8b310010e408c26671021fabeac7fcc45d6f90
gilson gi <gilsongillazaro@gmail.com>
2026-07-09 12:30:03 +0100

    integracao com analitics

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import "react-toastify/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <ToastContainer />
     <App />
+    <Analytics />
   </BrowserRouter>,
 );
